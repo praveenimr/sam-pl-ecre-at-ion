e3754910277f3ccea664ad50d6802dd07344a733
praveenimr <praveenchavan.imr@gmail.com>
2024-08-28 17:00:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 import streamlit as st
+
+# Add custom CSS to hide the GitHub icon
+hide_github_icon = """
+#GithubIcon {
+  visibility: hidden;
+}
+"""
+st.markdown(hide_github_icon, unsafe_allow_html=True)
 from docx import Document
 from pptx import Presentation
 import io
